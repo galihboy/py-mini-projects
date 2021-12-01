b0163c76efe60ab9827c4b76363ee4b8da2b2352
Galih Hermawan <galih.hermawan@gmail.com>
2021-12-01 17:35:48 +0700

    update - kesalahan metode alternatif

--- Faktor_Bilangan/faktorisasi_prima.py
+++ Faktor_Bilangan/faktorisasi_prima.py
@@ -6,34 +6,46 @@ import math
 from collections import Counter
 
 
+# fungsi untuk memeriksa apakah sebuah angka memiliki akar kuadrat dengan mengeluarkan nilai akarnya jika punya
+def akar_kuadrat(angka):
+    akar = math.sqrt(angka)
+    if int(akar+0.5) ** 2 == angka:
+        return int(akar)
+    else:
+        return None
+
 # menghasilkan faktor bilangan dari sebuah angka
 def faktor_bilangan(angka):
     listOutput = []
-    faktor = [i for i in range(1, angka + 1) if angka % i == 0]
-    jml = len(faktor) // 2 + len(faktor) % 2
+    faktor = [i for i in range(1,angka+1) if angka%i==0]
+    jml = len(faktor)//2 + len(faktor)%2
     for i, data in enumerate(faktor):
-        if i == jml: break
-        listOutput.append([faktor[i], faktor[-1 - i]])
+        if i==jml: break
+        listOutput.append([faktor[i], faktor[-1-i]])
     return faktor, listOutput
 
+# menghasilkan hasil faktorisasi prima dari sebuah angka
+def faktorisasi_prima(angka):
+    lstFaktorisasiPrima = []
+    lstTemp = [angka]
 
-# menimpa data list yang memiliki akar kuadrat dengan nilai akarnya
-def akar_kuadrat_v2(listAngka):
-    for angka in listAngka:
-        akar = math.sqrt(angka)
-        if int(akar + 0.5) ** 2 == angka:
-            yield int(akar)
+    while True:
+        angka = lstTemp[0]
+        akar = akar_kuadrat(angka)
+        if akar:
+            lstTemp.remove(angka)
+            lstTemp.extend([akar, akar])
         else:
-            yield angka
+            _, lstFaktor = faktor_bilangan(angka)
+            if len(lstFaktor)>1:
+                lstTemp.remove(angka)
+                lstTemp.extend(lstFaktor[1])
+            else: # prima
+                lstTemp.remove(angka)
+                lstFaktorisasiPrima.append(angka)
 
-
-# menyeleksi data list yang termasuk dalam bilangan prima
-def seleksi_prima(listAngka):
-    for angka in listAngka:
-        bilFaktor, hasilKali = faktor_bilangan(angka)
-        if len(hasilKali) == 1 and angka != 1:
-            yield angka
-
+        if not lstTemp: break
+    return lstFaktorisasiPrima
 
 # menyederhanakan tampilan hasil faktorisasi prima dengan bilangan prima berpangkat
 def kamus_penyederhanaan(listFaktorPrima):
@@ -47,20 +59,14 @@ def kamus_penyederhanaan(listFaktorPrima):
 
 
 if __name__ == '__main__':
-    angka = 100
+    angka = 90000
 
-    # 1. Membangkitkan bilangan faktor
     bilFaktor, hasilKali = faktor_bilangan(angka)
     print(f"Faktor bilangan dari angka {angka} adalah {bilFaktor}")
+    print(f"Kombinasi hasil perkalian: {hasilKali}")
 
-    # 2. Cari bilangan yang memiliki akar kuadrat, ganti angka lama dengan angka akar kuadratnya.
-    list_pasca_akar_kuadrat = [i for i in akar_kuadrat_v2(bilFaktor)]
-    print(f"Hasil penggantian bilangan yang memiliki akar kuadrat: {list_pasca_akar_kuadrat}")
+    hasil = faktorisasi_prima(angka)
+    print(f"Hasil faktorisasi prima dari angka {angka} adalah {hasil}")
 
-    # 3. Seleksi hanya yang termasuk bilangan prima
-    list_prima = [i for i in seleksi_prima(list_pasca_akar_kuadrat)]
-    print(f"List faktor prima: {list_prima}")
-
-    # 4. Penyederhanaan
-    strSederhana = " x ".join(kamus_penyederhanaan(list_prima))
+    strSederhana = " x ".join(kamus_penyederhanaan(hasil))
     print(f"Bentuk sederhana: {strSederhana}")
